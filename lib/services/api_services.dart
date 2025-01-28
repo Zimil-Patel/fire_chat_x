f6dcef7add9e1665ebd6f33bf7fe20a291e4ca51
Zimil-Patel <patelzimil2310@gmail.com>
2025-01-28 08:55:05 +0530

    28 Jan 2025

--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -9,8 +9,10 @@ class ApiServices{
   static final ApiServices apiServices = ApiServices._();
 
   final api = "https://api.imgur.com/3/image/";
-    final clientId = "bdde645e633548b";
-  final secretId = "f7b62d151ff6bb61e1055dc68a37b4aea7014958";
+    final clientId = "954428364df58d9";
+  final secretId = "c735c2d607fa1dd1ee44988020f30d01c02e0c8b";
+  final authorizationCode = "3a5bf3d7c48efc1408681a6b2aacd8ac01af1e2d";
+
 
   Future<String> postImage(Uint8List image) async {
     log("called postImage...");
@@ -35,4 +37,29 @@ class ApiServices{
 
     return "https://www.techsmith.com/blog/wp-content/uploads/2023/08/What-are-High-Resolution-Images.png";
   }
+
+
+  // get access token
+  Future<void> getAccessToken(String authCode) async {
+    final response = await http.post(
+      Uri.parse('https://api.imgur.com/oauth2/token'),
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      body: jsonEncode({
+        'client_id': clientId,
+        'client_secret': secretId,
+        'grant_type': authorizationCode,
+        'code': authCode,
+        'redirect_uri': 'http://localhost',
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      log("Access Token: ${data['access_token']}");
+      log("Refresh Token: ${data['refresh_token']}");
+    } else {
+      log("Error: ${response.body}");
+    }
+  }
 }
+
